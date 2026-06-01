2e35ef4c1031741888373bf146ebebbbc79f8749
Juan Yepes <juan.yepes@udla.edu.ec>
2026-06-01 11:19:41 -0500

    Creación de los bucles de ingreso de datos y la validación do-while (0-10)

--- sistema.c
+++ sistema.c
@@ -10,3 +10,17 @@ int main() {
 
     
     float notas[num_estudiantes][num_asignaturas];
+
+ 
+    for(int i = 0; i < num_estudiantes; i++) {
+        printf("\n>> Estudiante %d <<\n", i + 1);
+        for(int j = 0; j < num_asignaturas; j++) {
+            do {
+                printf("Ingrese la calificacion para la Asignatura %d (0 a 10): ", j + 1);
+                scanf("%f", &notas[i][j]);
+                if(notas[i][j] < 0 || notas[i][j] > 10) {
+                    printf("Error: La calificacion es invalida. Intente de nuevo.\n");
+                }
+            } while(notas[i][j] < 0 || notas[i][j] > 10);
+        }
+    }   
