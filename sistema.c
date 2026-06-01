8d24d1cb3e060e0fadbef5561ef8f581f4733fee
alx4ndre <mateo.mero@udla.edu.ec>
2026-06-01 11:28:53 -0500

    Implementación de la lógica para sumar y sacar los promedios horizontales y verticales.

--- sistema.c
+++ sistema.c
@@ -24,3 +24,19 @@ int main() {
             } while(notas[i][j] < 0 || notas[i][j] > 10);
         }
     }   
+    
+    float prom_estudiante[num_estudiantes];
+    float prom_asignatura[3] = {0, 0, 0};
+
+    for(int i = 0; i < num_estudiantes; i++) {
+        float suma_estudiante = 0;
+        for(int j = 0; j < num_asignaturas; j++) {
+            suma_estudiante += notas[i][j];
+            prom_asignatura[j] += notas[i][j];
+        }
+        prom_estudiante[i] = suma_estudiante / num_asignaturas;
+    }
+
+    for(int j = 0; j < num_asignaturas; j++) {
+        prom_asignatura[j] = prom_asignatura[j] / num_estudiantes;
+    }
